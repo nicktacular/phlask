87f06e25bb908f67194db66c51dd843e9c91c550
Nick Ilyin <nick.ilyin@gmail.com>
2013-10-09 17:39:24 -0400

    Composer, initial MemoryQueue class Basically for testing rather than any real world use

--- lib/phlask/TaskQueue/MemoryQueue.php
+++ lib/phlask/TaskQueue/MemoryQueue.php
@@ -12,10 +12,13 @@
  * @link     http://github.com/nicktacular/phlask
  */
 
-namespace phlask;
+namespace phlask\TaskQueue;
+
+use phlask\TaskQueueInterface;
+use phlask\TaskSpecInterface;
 
 /**
- * A simple interface queueing up tasks
+ * A simple in-memory queue
  *
  * @category Phlask
  * @package  TaskRunner
@@ -23,28 +26,48 @@ namespace phlask;
  * @license  http://opensource.org/licenses/MIT MIT
  * @link     http://github.com/nicktacular/phlask
  */
-interface TaskQueueInterface
+class MemoryQueue implements TaskQueueInterface
 {
+    /**
+     * Holds an array of TaskSpecInterface objects
+     *
+     * @var array
+     */
+    protected $queue = array();
+
     /**
      * Checks if any tasks exist.
      *
      * @return bool True if there are any tasks.
      */
-    public function hasTasks();
+    public function hasTasks()
+    {
+        return count($this->queue) > 0;
+    }
 
     /**
      * Retrieves a number of remaining tasks in the queue.
      *
      * @return int The number of tasks (can be zero).
      */
-    public function count();
+    public function count()
+    {
+        return count($this->queue);
+    }
 
     /**
      * Retrieve the next task off the queue.
      *
-     * @return TaskSpecInterface|null The next task or null if none.
+     * @return TaskSpecInterface The next task.
      */
-    public function popTask();
+    public function popTask()
+    {
+        if (empty($this->queue)) {
+            return null;
+        }
+
+        return array_shift($this->queue);
+    }
 
     /**
      * Add a task to the queue.
@@ -52,5 +75,8 @@ interface TaskQueueInterface
      * @param  TaskSpecInterface $task The task to add.
      * @return null
      */
-    public function pushTask(TaskSpecInterface $task);
+    public function pushTask(TaskSpecInterface $task)
+    {
+        $this->queue[] = $task;
+    }
 }
