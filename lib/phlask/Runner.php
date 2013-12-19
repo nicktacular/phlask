07a695e0f1e7596792d8a527182786cd9fe85b29
Nick Ilyin <nick.ilyin@gmail.com>
2013-12-19 17:33:13 -0500

    Now supports max procs

--- lib/phlask/Runner.php
+++ lib/phlask/Runner.php
@@ -11,7 +11,7 @@ class Runner
     /**
      * A hash of running tasks.
      *
-     * @var SplObjectStorage Hash of resources opened by proc_open().
+     * @var SplObjectStorage Stores Task objects.
      */
     protected $runningTasks;
 
@@ -54,11 +54,21 @@ class Runner
     protected $logger;
 
     /**
+     * Specifies the max number of concurrently running processes.
+     *
+     * @var int
+     */
+    protected $maxProcesses = 10;
+
+    /**
      * Creation of a runner instance.
      *
      * @param array $config The configuration for this instance.
      *
-     * @return Runner An instance which can be activated by calling ->run().
+     * @return Runner
+     *
+     * @throws Exception\InvalidTaskQueueException
+     * @throws Exception\InvalidArgumentException
      */
     public static function factory(array $config = [])
     {
@@ -85,9 +95,17 @@ class Runner
             $config['daemon'] = (bool) $config['daemon'];
         }
 
+        if (!isset($config['max_processes']) || !is_int($config['max_processes']) || $config['max_processes'] < 1) {
+            throw new Exception\InvalidArgumentException(
+                "The 'max_processes' must be specified as a positive integer."
+            );
+        }
+
         //verbosity level
-        if (isset($config['verbose']) && $config['verbose'] == true) {
-            $this->verbose = true;
+        if (!isset($config['verbose'])) {
+            $config['verbose'] = false;
+        } else {
+            $config['verbose'] = (bool) $config['verbose'];
         }
 
         if (isset($config['logger']) && $config['logger'] instanceof LoggerInterface) {
@@ -96,10 +114,24 @@ class Runner
             $logger = new NullLogger;
         }
 
+        if (isset($config['task_storage']) && $config['task_storage'] instanceof SplObjectStorage) {
+            $store = $config['task_storage'];
+        } else {
+            $store = new SplObjectStorage;
+        }
+
         //php configs
         set_time_limit(0);
 
-        return new self($config['tasks'], $config['daemon'], $config['wait'], $logger);
+        return new self(
+            $config['tasks'],
+            $config['daemon'],
+            $config['wait'],
+            $config['verbose'],
+            $config['max_processes'],
+            $logger,
+            $store
+        );
     }
 
     /**
@@ -113,34 +145,53 @@ class Runner
      *                                  to run and act on items in the queue when
      *                                  they appear (eventually).
      * @param int             $wait   The number of µs to wait to sample a process.
+     * @param bool            $verbose Whether to run verbosely.
+     * @param int             $maxProcesses Maximum number of concurrently running tasks.
      * @param LoggerInterface $logger A logger to output information about what's
      *                                  going on in this world.
+     * @param SplObjectStorage $taskStore A means for storing running tasks.
      */
     public function __construct(
         TaskQueueInterface $tasks,
         $daemonMode,
         $wait,
-        LoggerInterface $logger
+        $verbose,
+        $maxProcesses,
+        LoggerInterface $logger,
+        SplObjectStorage $taskStore
     ) {
         $this->tasks        = $tasks;
         $this->daemonMode   = $daemonMode;
         $this->wait         = $wait;
-        $this->runningTasks = new SplObjectStorage;
+        $this->verbose      = $verbose;
         $this->logger       = $logger;
+        $this->maxProcesses = $maxProcesses;
+        $this->runningTasks = $taskStore;
     }
 
     //@todo catch the exceptions necessary in this method
     public function run()
     {
         while (1) {
-            while ($this->tasks->hasTasks()) {
+            while ($this->tasks->hasTasks() && $this->runningTasks->count() <= $this->maxProcesses) {
                 $this->logger->info("Have " . $this->tasks->count() . ' tasks to start');
+
                 //pop a task, init and run
                 $taskSpec = $this->tasks->popTask();
                 $task = Task::factory($taskSpec);
                 $this->runningTasks->attach($task);
-                $task->run();//@throws Exception\ExecutionException
-                $this->logger->info($task->getTaskSpec()->getName() . '(' . $task->getPid() . ')');
+
+                try {
+                    $task->run();
+                    $this->logger->info($task->getTaskSpec()->getName() . '(' . $task->getPid() . ')');
+                } catch (Exception\ExecutionException $e) {
+                    $this->logger->warning($e->getMessage());
+                    $this->runningTasks->detach($task);
+                }
+            }
+
+            if ($this->tasks->hasTasks() && $this->runningTasks->count() == $this->maxProcesses) {
+                $this->logger->info("Reached maximum concurrency with {$this->maxProcesses} processes.");
             }
 
             //sleep so we poll later
