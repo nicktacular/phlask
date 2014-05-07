c857c8e639f8264640c99c9cdc0c9566f9e61ba7
Nick Ilyin <nick.ilyin@gmail.com>
2014-05-06 20:22:35 -0400

    New bash script to test trapping of signals

---
+++ tests/fixtures/trappableScript.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+dest=$1
+sleepFor=$2
+
+echo "Started" > "$dest"
+
+function writeToFile {
+    echo "$1" > "$dest"
+    exit $2
+}
+
+trap 'writeToFile "SIGTERM" 7' SIGTERM
+trap 'writeToFile "SIGINT" 8' SIGINT
+
+loop=1
+while [ $loop = 1 ];
+do
+    sleep 1
+done
+
